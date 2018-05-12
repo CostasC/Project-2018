338c9940477694ae752d10460812ef813b81afea
Kostas <Kostas>
2018-05-12 13:56:40 +0300

    New Change, just a print on the main method

---
+++ SchoolApp-master/src/shapp/ShApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shapp;
+
+/**
+ *
+ * @author Joanna P
+ */
+public class ShApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My Sch App");
+        System.out.println("Test Changes");
+        System.out.println("Hello Team, this is a change on GitHub");
+    }
+    
+}
